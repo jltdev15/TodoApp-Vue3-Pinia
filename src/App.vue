d56eb3f96c3c9a906be358af325305263f208ade
jltdev15 <75570239+jltdev15@users.noreply.github.com>
2024-01-30 11:26:29 +0800

    fix the mobile layout

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 
 <template>
-  <section class="flex items-center justify-center bg-gray-200 sm:py-12 md:h-screen">
+  <section class="flex justify-center h-screen bg-gray-200 sm:items-center sm:py-12">
     <router-view> </router-view>
   </section>
 </template>
